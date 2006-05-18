4cc49a178d8fa85d42f63eefe77e4c7976bd1faa
luke <luke@c9fdbc50-1c0f-0410-8134-ac6df33ab1bd>
2006-05-18 13:07:59 +0000

    Up'ed version to 0.5
    git-svn-id: svn://svn.xara.com/Trunk/XaraLX@1115 c9fdbc50-1c0f-0410-8134-ac6df33ab1bd

--- wxOil/staticversion.h
+++ wxOil/staticversion.h
@@ -95,10 +95,10 @@ service marks of Xara Group Ltd. All rights in these marks are reserved.
 
 =================================XARAHEADEREND============================
  */
-const TCHAR g_pszAppVersion[] = wxT("0.4");
+const TCHAR g_pszAppVersion[] = wxT("0.5");
 
 #define	CAMELOT_VERSION_MAJOR	0
-#define	CAMELOT_VERSION_MINOR	4
-#define	CAMELOT_VERSION			0.4
-#define	CAMELOT_VERSION_STRING	wxT("0.4 (root)")
+#define	CAMELOT_VERSION_MINOR	5
+#define	CAMELOT_VERSION			0.5
+#define	CAMELOT_VERSION_STRING	wxT("0.5 (root)")
 #define	MASTER_BUILDER	0
