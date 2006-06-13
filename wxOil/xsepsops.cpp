c39e6c2f8543eacb47f3ee9819671fe76ba23b47
alex <alex@c9fdbc50-1c0f-0410-8134-ac6df33ab1bd>
2006-06-13 10:58:53 +0000

    Ran normalise.pl on 2 new files
    git-svn-id: svn://svn.xara.com/Trunk/XaraLX@1308 c9fdbc50-1c0f-0410-8134-ac6df33ab1bd

--- wxOil/xsepsops.cpp
+++ wxOil/xsepsops.cpp
@@ -1,4 +1,100 @@
-// $Header: /Camelot/winoil/xsepsops.cpp 2     23/03/04 12:01p Alex $
+// $Id: wxOil/xsepsops.cpp, 1, 01-Jan-2006, Anonymous $
+/* @@tag:xara-cn@@ DO NOT MODIFY THIS LINE
+================================XARAHEADERSTART===========================
+ 
+               Xara LX, a vector drawing and manipulation program.
+                    Copyright (C) 1993-2006 Xara Group Ltd.
+       Copyright on certain contributions may be held in joint with their
+              respective authors. See AUTHORS file for details.
+
+LICENSE TO USE AND MODIFY SOFTWARE
+----------------------------------
+
+This file is part of Xara LX.
+
+Xara LX is free software; you can redistribute it and/or modify it
+under the terms of the GNU General Public License version 2 as published
+by the Free Software Foundation.
+
+Xara LX and its component source files are distributed in the hope
+that it will be useful, but WITHOUT ANY WARRANTY; without even the
+implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along
+with Xara LX (see the file GPL in the root directory of the
+distribution); if not, write to the Free Software Foundation, Inc., 51
+Franklin St, Fifth Floor, Boston, MA  02110-1301 USA
+
+
+ADDITIONAL RIGHTS
+-----------------
+
+Conditional upon your continuing compliance with the GNU General Public
+License described above, Xara Group Ltd grants to you certain additional
+rights. 
+
+The additional rights are to use, modify, and distribute the software
+together with the wxWidgets library, the wxXtra library, and the "CDraw"
+library and any other such library that any version of Xara LX relased
+by Xara Group Ltd requires in order to compile and execute, including
+the static linking of that library to XaraLX. In the case of the
+"CDraw" library, you may satisfy obligation under the GNU General Public
+License to provide source code by providing a binary copy of the library
+concerned and a copy of the license accompanying it.
+
+Nothing in this section restricts any of the rights you have under
+the GNU General Public License.
+
+
+SCOPE OF LICENSE
+----------------
+
+This license applies to this program (XaraLX) and its constituent source
+files only, and does not necessarily apply to other Xara products which may
+in part share the same code base, and are subject to their own licensing
+terms.
+
+This license does not apply to files in the wxXtra directory, which
+are built into a separate library, and are subject to the wxWindows
+license contained within that directory in the file "WXXTRA-LICENSE".
+
+This license does not apply to the binary libraries (if any) within
+the "libs" directory, which are subject to a separate license contained
+within that directory in the file "LIBS-LICENSE".
+
+
+ARRANGEMENTS FOR CONTRIBUTION OF MODIFICATIONS
+----------------------------------------------
+
+Subject to the terms of the GNU Public License (see above), you are
+free to do whatever you like with your modifications. However, you may
+(at your option) wish contribute them to Xara's source tree. You can
+find details of how to do this at:
+  http://www.xaraxtreme.org/developers/
+
+Prior to contributing your modifications, you will need to complete our
+contributor agreement. This can be found at:
+  http://www.xaraxtreme.org/developers/contribute/
+
+Please note that Xara will not accept modifications which modify any of
+the text between the start and end of this header (marked
+XARAHEADERSTART and XARAHEADEREND).
+
+
+MARKS
+-----
+
+Xara, Xara LX, Xara X, Xara X/Xtreme, Xara Xtreme, the Xtreme and Xara
+designs are registered or unregistered trademarks, design-marks, and/or
+service marks of Xara Group Ltd. All rights in these marks are reserved.
+
+
+      Xara Group Ltd, Gaddesden Place, Hemel Hempstead, HP2 6EX, UK.
+                        http://www.xara.com/
+
+=================================XARAHEADEREND============================
+ */
 //
 // XSEPSExportOptions - implements the options relevant to the export of XS EPS
 
@@ -7,11 +103,11 @@
 
 #include "camtypes.h"
 #include "xsepsops.h"
-#include "printdlg.h"
+//#include "printdlg.h"
 #include "mainfrm.h"
 #include "epsfiltr.h"
-#include "markn.h"
-#include "resource.h"
+//#include "markn.h"
+//#include "resource.h"
 #include "helpuser.h"
 
 DECLARE_SOURCE("$Revision: 2 $");
@@ -20,7 +116,7 @@ DECLARE_SOURCE("$Revision: 2 $");
 
 BEGIN_MESSAGE_MAP(XSEPSExportOptions,CDialog)
 	//{{AFX_MSG_MAP(CPrintDialog)
-	ON_COMMAND_EX(IDC_XSEPSOPSHELP,	OnCommand)
+	ON_COMMAND_EX(_R(IDC_XSEPSOPSHELP),	OnCommand)
 	//}}AFX_MSG_MAP
 END_MESSAGE_MAP()
 
@@ -34,7 +130,7 @@ IMPLEMENT_DYNAMIC(XSEPSExportOptions,CDialog)
 
 >	XSEPSExportOptions::XSEPSExportOptions()
 
-	Author:		Markn
+	Author:		Mark_Neves (Xara Group Ltd) <camelotdev@xara.com>
 	Created:	30/5/95
 	Inputs:		-
 	Returns:	-
@@ -43,7 +139,7 @@ IMPLEMENT_DYNAMIC(XSEPSExportOptions,CDialog)
 
 ********************************************************************************************/
 
-XSEPSExportOptions::XSEPSExportOptions() : CDialog(IDD_XSEPSOPS, GetMainFrame())
+XSEPSExportOptions::XSEPSExportOptions() : CDialog(_R(IDD_XSEPSOPS), GetMainFrame())
 {
 }
 
@@ -52,7 +148,7 @@ XSEPSExportOptions::XSEPSExportOptions() : CDialog(IDD_XSEPSOPS, GetMainFrame())
 
 >	BOOL XSEPSExportOptions::OnInitDialog()
 
-	Author:		Markn
+	Author:		Mark_Neves (Xara Group Ltd) <camelotdev@xara.com>
 	Created:	30/5/95
 	Inputs:		-
 	Returns:	TRUE if OK, FALSE otherwise
@@ -66,16 +162,16 @@ BOOL XSEPSExportOptions::OnInitDialog()
 	if (CDialog::OnInitDialog())
 	{
 		TCHAR s[256];
-		wsprintf(s, TEXT("%d"), (int) EPSFilter::XSEPSExportDPI);
-		SetDlgItemText(IDC_DPIEDIT, s);
+		wsprintf(s, TEXT("%d"), (INT32) EPSFilter::XSEPSExportDPI);
+		SetDlgItemText(_R(IDC_DPIEDIT), s);
 
 		/*
-		CheckDlgButton(IDC_PSLEVELAUTO,	EPSFilter::XSEPSExportPSType == 0);
-		CheckDlgButton(IDC_PSLEVEL1,	EPSFilter::XSEPSExportPSType == 1);
-		CheckDlgButton(IDC_PSLEVEL2,	EPSFilter::XSEPSExportPSType == 2);
+		CheckDlgButton(_R(IDC_PSLEVELAUTO),	EPSFilter::XSEPSExportPSType == 0);
+		CheckDlgButton(_R(IDC_PSLEVEL1),	EPSFilter::XSEPSExportPSType == 1);
+		CheckDlgButton(_R(IDC_PSLEVEL2),	EPSFilter::XSEPSExportPSType == 2);
 		*/
 
-		CheckDlgButton(IDC_EXPORTTEXTASCURVES, EPSFilter::XSEPSExportTextAsCurves);
+		CheckDlgButton(_R(IDC_EXPORTTEXTASCURVES), EPSFilter::XSEPSExportTextAsCurves);
 
 		return TRUE;
 	}
@@ -87,7 +183,7 @@ BOOL XSEPSExportOptions::OnInitDialog()
 
 >	void XSEPSExportOptions::OnOK()
 
-	Author:		Markn
+	Author:		Mark_Neves (Xara Group Ltd) <camelotdev@xara.com>
 	Created:	31/5/95
 	Inputs:		-
 	Returns:	-
@@ -101,7 +197,7 @@ void XSEPSExportOptions::OnOK()
 {
 	// DPI ed field
 	BOOL TranslatedOK;
-	UINT DPI = GetDlgItemInt(IDC_DPIEDIT,&TranslatedOK,FALSE);
+	UINT32 DPI = GetDlgItemInt(_R(IDC_DPIEDIT),&TranslatedOK,FALSE);
 	if (!TranslatedOK || DPI < 10)
 		DPI = 10;
 
@@ -110,24 +206,24 @@ void XSEPSExportOptions::OnOK()
 
 	if (DPI > 300)
 	{
-		int b = InformWarning(IDS_EXPORT_BIGDPI,IDS_OK,IDS_CANCEL);
+		INT32 b = InformWarning(_R(IDS_EXPORT_BIGDPI),_R(IDS_OK),_R(IDS_CANCEL));
 		if (b != 1)
 			return;
 	}
 
 	/*
 	// Job 10463: remove PS Level bits - default to Level 2
-	int ps = 0;
-	if (IsDlgButtonChecked(IDC_PSLEVEL1))
+	INT32 ps = 0;
+	if (IsDlgButtonChecked(_R(IDC_PSLEVEL1)))
 		ps = 1;
-	if (IsDlgButtonChecked(IDC_PSLEVEL2))
+	if (IsDlgButtonChecked(_R(IDC_PSLEVEL2)))
 		ps = 2;
 	*/
 
 	EPSFilter::XSEPSExportDPI 	 = DPI;
 	// EPSFilter::XSEPSExportPSType = ps;
 
-	EPSFilter::XSEPSExportTextAsCurves = IsDlgButtonChecked(IDC_EXPORTTEXTASCURVES);
+	EPSFilter::XSEPSExportTextAsCurves = IsDlgButtonChecked(_R(IDC_EXPORTTEXTASCURVES));
 
 	CDialog::OnOK();
 }
@@ -136,7 +232,7 @@ void XSEPSExportOptions::OnOK()
 
 >	BOOL XSEPSExportOptions::OnCommand()
 
-	Author:		Markn
+	Author:		Mark_Neves (Xara Group Ltd) <camelotdev@xara.com>
 	Created:	1/6/95
 	Inputs:		-
 	Returns:	TRUE if all ok, FALSE if an error occurs
@@ -148,11 +244,11 @@ void XSEPSExportOptions::OnOK()
 
 ********************************************************************************************/
 
-BOOL XSEPSExportOptions::OnCommand(UINT GadgetID)
+BOOL XSEPSExportOptions::OnCommand(UINT32 GadgetID)
 {
 	switch (GadgetID)
 	{
-		case IDC_XSEPSOPSHELP:
+		case _R(IDC_XSEPSOPSHELP):
 			HelpUser(*this);
 			break;
 	}
