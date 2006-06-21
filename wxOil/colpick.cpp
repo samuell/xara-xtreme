28010c6ef3118116a79d321385612137f8d446d9
alex <alex@c9fdbc50-1c0f-0410-8134-ac6df33ab1bd>
2006-06-21 16:56:37 +0000

    Only resize the sizer if it exists
    git-svn-id: svn://svn.xara.com/Trunk/XaraLX@1349 c9fdbc50-1c0f-0410-8134-ac6df33ab1bd

--- wxOil/colpick.cpp
+++ wxOil/colpick.cpp
@@ -3108,7 +3108,9 @@ void ColourPicker::OnCreate(CWindowID WindowID)
 		pPicker->SetSize(pPicker->GetSize().GetWidth()+1, pPicker->GetSize().GetHeight()+1); // cause a resize
 		WindowID->Layout();
 		WindowID->Fit();
-		WindowID->GetSizer()->SetSizeHints(WindowID);
+		wxSizer *pSizer = WindowID->GetSizer();
+		if (pSizer)
+			pSizer->SetSizeHints(WindowID);
 		pPicker->SetMinSize(s_UserSize);
 		pPicker->SetMaxSize(s_UserSize);
 		pPicker->SetSize(s_UserSize);
