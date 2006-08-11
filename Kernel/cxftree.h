bd6333ea65f1680cc62f45a99a6e34dbce594e7e
alex <alex@c9fdbc50-1c0f-0410-8134-ac6df33ab1bd>
2006-08-11 18:04:37 +0000

    Fix gcc 4.1 build
    git-svn-id: svn://svn.xara.com/Trunk/XaraLX@1693 c9fdbc50-1c0f-0410-8134-ac6df33ab1bd

--- Kernel/cxftree.h
+++ Kernel/cxftree.h
@@ -287,7 +287,7 @@ private:
 
 	INT32 AddDisplayNode(CXFNode* pNode,INT32 Index,INT32 Depth,BOOL ExpandAll);
 	void ShowNodeDebugInfoForNode(CXFNode *pNode);
-	CXaraFileRecordHandler* CXFTreeDlg::FindHandler(UINT32 Tag);
+	CXaraFileRecordHandler* FindHandler(UINT32 Tag);
 	void GetTagText(CXFNode* pNode,String_256& Str);
 
 	CXFNode* pRoot;
